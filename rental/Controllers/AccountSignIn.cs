1c2cc6b26cffde3d2904ab2ffc1897323b8dea4a
yslyslysl <hp@DESKTOP-N22ISGA>
2023-12-30 13:20:13 +0800

    Add project files.

---
+++ rental/Controllers/AccountSignIn.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace rental.Controllers
+{
+    public class AccountSignIn : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult TenantSignIn()
+        {
+            //logic ari
+            return View();
+        }
+        public IActionResult AdminSignIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ rental/Controllers/Application.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace rental.Controllers
+{
+    public class Application : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
